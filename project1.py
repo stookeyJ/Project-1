513739c83e71a0b04f39b46c1fdb68a033d6511b
stookeyJ <166753185+stookeyJ@users.noreply.github.com>
2024-04-11 15:20:46 -0400

    Initial commit

---
+++ project1.py
@@ -0,0 +1,34 @@
+#Author: 
+#Project Name: 
+#Date Created: 
+#Description: 
+#             
+
+import math
+
+def quadrant_check(p1, p2): #TODO
+    pass #Replace pass with your code
+
+def find_distance(p1, p2): #TODO
+    pass #Replace pass with your code
+
+def cont(): #TODO
+    pass #Replace pass with your code
+
+def main():
+    pass #Replace pass with your code
+
+    #TODO
+    #x = 
+    #y = 
+    #coordinate = 
+
+    #DONE
+    #print("The point is located in", quadrant_check(x, y), coordinate) #Uses the return value of quadrant_check() to print the quadrant
+    
+    #print("The distance from the origin is: ", find_distance(x, y)) #Uses the return of find_distance to print the distance from origin
+    
+    #cont() #Calls the cont() function to see if the user would like to continue continue the program or end
+
+
+main() #Only function call. DO NOT CHANGE THIS
